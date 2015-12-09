54decfc9a4e2a33666d3bf96905f327c087d6270
Matthew Conto <mbsconto@gmail.com>
2015-12-09 14:03:08 -0500

    gitignore, outline

---
+++ Assets/Scripts/ParkManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParkManager : MonoBehaviour {
+	
+	[HeaderAttribute("Status")]
+	public float time;
+	public int month;
+	public float progression;
+	[HeaderAttribute("Setup")]
+	public Tile tilePrefab;
+	public Trash trashPrefab;
+	
+	[HeaderAttribute("Balance")]
+	public float timePerMonth;
+	 
+	
+	[HeaderAttribute("UI")]
+	public Text uiTextOutput;
+	
+	private List<Tile> tiles;
+	private List<Trash> trash;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
